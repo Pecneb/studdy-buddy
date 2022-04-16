b838f557de52e84cf3aaad5ae3941edacb075aed
Pecneb <ecneb2000@gmail.com>
2022-04-16 22:23:40 +0200

    started working on app

---
+++ studdybuddy/auth.py
@@ -0,0 +1,100 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from studdybuddy.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        neptun = request.form['neptun']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        password = request.form['password']
+        email = request.form['email']
+        db = get_db()
+        error = None
+
+        if not neptun:
+            error = 'Neptun is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif not email:
+            error = 'Email is required.'
+
+        if error is None:
+            try:
+                db.exeute(
+                    "INSERT INTO hallgato (neptun, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)",
+                    (neptun, firstname, lastname, email, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User with {neptun} neptun-code is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+        
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', method=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        neptun = request.form['neptun']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM hallgato WHERE neptun = ?', (neptun,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect neptun-code.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['neptun'] = user['neptun']
+            # TODO: this will not return to the index page, rather to the users main page
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    neptun = session.get('neptun')
+
+    if neptun is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM hallgato WHERE neptun = ?', (neptun,)
+        ).fetchone()
+
+
+@bo.route('/logout')
+def logout():
+    session.clear()
+    # TODO: not sure where to redirect, there is no index yet, but who knows
+    # if there will be none, then this should redirect to the /auth/login page
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
---
+++ studdybuddy/schema.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS hallgato;
+DROP TABLE IF EXISTS tanulotars;
+DROP TABLE IF EXISTS csoport;
+DROP TABLE IF EXISTS korrepetalas;
+DROP TABLE IF EXISTS korrepetalas_resztvevo;
+DROP TABLE IF EXISTS csoport_tag;
+DROP TABLE IF EXISTS tantargy;
+
+CREATE TABLE hallgato (
+  neptun   varchar(6) NOT NULL, 
+  firstname     varchar(255) NOT NULL, 
+  lastname     varchar(255) NOT NULL,
+  email    varchar(255) NOT NULL, 
+  password varchar(255) NOT NULL, 
+  PRIMARY KEY (neptun));
+
+CREATE TABLE tanulotars (
+  hallgatoneptun  varchar(6) NOT NULL, 
+  hallgatoneptun2 varchar(6) NOT NULL, 
+  PRIMARY KEY (hallgatoneptun, 
+  hallgatoneptun2));
+
+CREATE TABLE tantargy (
+  tkod varchar(255) NOT NULL, 
+  tnev varchar(255) NOT NULL, 
+  PRIMARY KEY (tkod));
+
+CREATE TABLE korrepetalas (
+  id                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+  korrepetaloneptun varchar(6) NOT NULL, 
+    korrepetalasnev   varchar(255) NOT NULL, 
+  tkod              varchar(255) NOT NULL, 
+  tol               date NOT NULL, 
+  ig                date NOT NULL);
+
+CREATE TABLE korrepetalas_resztvevo (
+  hallgatoneptun varchar(6) NOT NULL, 
+  korrepetalasid integer(10) NOT NULL, 
+  FOREIGN KEY(hallgatoneptun) REFERENCES hallgato(neptun),
+  FOREIGN KEY(korrepetalasid) REFERENCES korrepetalas(id));
+
+CREATE TABLE csoport (
+  id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+  name varchar(255));
+
+CREATE TABLE csoport_tag (
+  hallgatoneptun varchar(6) NOT NULL, 
+  csoportid      integer(10) NOT NULL, 
+  admin          integer(1) NOT NULL,
+  FOREIGN KEY(hallgatoneptun) REFERENCES hallgato(neptun), 
+  FOREIGN KEY(csoportid) REFERENCES csoport(id));
+
