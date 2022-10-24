2404135549893b672c42fb88d744293e8a5af0ed
Bozsoki Levente <levbozsoki@gmail.com>
2022-10-24 11:48:29 +0200

    Revert "Merge remote-tracking branch 'origin/pecneb_workbranch' into dev-bozsi"
    This reverts commit 5ba41eb9330cc5a6b960a0a22b4b04d076bfe5c9, reversing
    changes made to fed720d3fde310f98721c45f5a1268b4480c5a17.

--- studdybuddy/__init__.py
+++ studdybuddy/__init__.py
@@ -1,8 +1,5 @@
 import os
-from re import template
 from flask import Flask, render_template
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 
 def page_not_found(e):
   return render_template('404.html'), 404
@@ -13,7 +10,6 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.join(app.instance_path, 'studdybuddy.db')}",
-        FLASK_ADMIN_SWATCH = 'darkly'
     )
 
     if test_config is None:
@@ -26,11 +22,8 @@ def create_app(test_config=None):
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
-        print(app.instance_path)
     except OSError:
         pass
-
-    admin = Admin(app, name='studdybuddy', template_mode='bootstrap3')
 
     app.register_error_handler(404, page_not_found)
 
@@ -38,16 +31,6 @@ def create_app(test_config=None):
     db.DB.init_app(app)
     with app.app_context():
         db.DB.create_all()
-    
-    from . import admin as libadmin
-    
-    admin.add_view(libadmin.StudentModelView(db.Student, db.DB.session))
-    admin.add_view(libadmin.PostModelView(db.Post, db.DB.session))
-    admin.add_view(libadmin.SubjectModelView(db.Subject, db.DB.session))
-    admin.add_view(libadmin.TutoringModelView(db.Tutoring, db.DB.session))
-    admin.add_view(libadmin.TutoringParticipantModelView(db.TutoringParticipant, db.DB.session))
-    admin.add_view(libadmin.GroupModelView(db.Group, db.DB.session))
-    admin.add_view(libadmin.GroupMemberModelView(db.GroupMember, db.DB.session))
 
     from . import auth
     app.register_blueprint(auth.bp)
@@ -58,11 +41,5 @@ def create_app(test_config=None):
 
     from . import find_partner
     app.register_blueprint(find_partner.bp)
-    
-    from . import profile
-    app.register_blueprint(profile.bp)
-
-    from . import find_group
-    app.register_blueprint(find_group.bp)
 
     return app
--- studdybuddy/db.py
+++ studdybuddy/db.py
@@ -15,11 +15,6 @@ class Student(DB.Model):
     password = DB.Column(DB.String(255), nullable=False)
     email = DB.Column(DB.String(255), nullable=False)
 
-class Relations(DB.Model):
-    id = DB.Column(DB.Integer, autoincrement=True, primary_key=True)
-    neptun1 = DB.Column(DB.String(6), ForeignKey("student.neptun"), nullable=False)
-    neptun2 = DB.Column(DB.String(6), ForeignKey("student.neptun"), nullable=False)
-
 class Post(DB.Model):
     __tablename__ = "post"
     id = DB.Column(DB.Integer, autoincrement=True, primary_key=True)
@@ -34,18 +29,13 @@ class Subject(DB.Model):
     id = DB.Column(DB.String(255), primary_key=True)
     name = DB.Column(DB.String(255), nullable=False)
 
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
 class Tutoring(DB.Model):
     __tablename__ = "tutoring"
     id = DB.Column(DB.Integer, autoincrement=True, primary_key=True)
     tutor_neptun = DB.Column(DB.String(6), ForeignKey("student.neptun"), nullable=False)
     tutoring_name = DB.Column(DB.String(255), nullable=False)
     subject_id = DB.Column(DB.String(255), ForeignKey("subject.id"), nullable=False)
-    created = DB.Column(DB.DateTime(timezone=True), default=datetime.now())
-    start_datetime = DB.Column(DB.DateTime(timezone=True), default=datetime.now())
+    start_datetime = DB.Column(DB.DateTime(timezone=True), default=datetime.utcnow())
     end_datetime = DB.Column(DB.DateTime(timezone=True), nullable=False)
 
 class TutoringParticipant(DB.Model):
@@ -58,27 +48,10 @@ class Group(DB.Model):
     __tablename__ = "group"
     id = DB.Column(DB.Integer, autoincrement=True, primary_key=True)
     name = DB.Column(DB.String(255), nullable=False)
-    desc = DB.Column(DB.String(500), nullable=False)
-    team_size = DB.Column(DB.Integer, nullable=False)
-    creator_neptun = DB.Column(DB.String(6), ForeignKey("student.neptun"), nullable=False)
-    subject_id = DB.Column(DB.String(255), ForeignKey("subject.id"), nullable=False)
     created = DB.Column(DB.DateTime(timezone=True), default=datetime.now())
-
-    def __init__(self, name, desc, team_size, cneptun, sub_id):
-        self.name = name
-        self.desc = desc
-        self.team_size = team_size
-        self.creator_neptun = cneptun
-        self.subject_id = sub_id
 
 class GroupMember(DB.Model):
     __tablename__ = "group_member"
     id = DB.Column(DB.Integer, autoincrement=True, primary_key=True)
     student_neptun = DB.Column(DB.String(6), ForeignKey("student.neptun"), nullable=False)
     group_id = DB.Column(DB.Integer, ForeignKey("group.id"), nullable=False)
-    admin = DB.Column(DB.Boolean, default=False, nullable=False)
-
-    def __init__(self, student_neptun, group_id, admin=False):
-        self.student_neptun = student_neptun
-        self.group_id = group_id
-        self.admin = admin
